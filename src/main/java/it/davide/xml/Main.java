9b08d1550f66cd53b6e765f9b15f36275cd93d4d
scaramellini <scar00davi@gmail.com>
2026-02-24 10:42:34 +0100

    prova commit

--- src/main/java/it/davide/xml/Main.java
+++ src/main/java/it/davide/xml/Main.java
@@ -69,6 +69,7 @@ public class Main {
                             new MultiLevelMasterDetailPattern() */
                             //new PreloadedFieldsPattern()
                             new PreassignedSelectionFieldsPattern()
+                            
                     )
             );
 
